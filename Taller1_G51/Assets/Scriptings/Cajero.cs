451f4980fd0424171a05a5d7deb41d9361397dbd
Sebastian <juan_s.vasquez_c@uao.edu.co>
2025-09-01 11:07:48 -0500

    Updated .gitignore - Added Cajero.cs

--- Taller1_G51/Assets/Scriptings/Cajero.cs
+++ Taller1_G51/Assets/Scriptings/Cajero.cs
@@ -1,16 +1,44 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cajero : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int id; 
+    public bool estaLibre = true;
+    public int clientesAtendidos = 0;
+    public float tiempoTotalAtencion = 0f;
+
+    public Image panelVisual; 
+    public Text estadoTexto; 
+
+    private Cliente clienteActual;
+
+    public void ActualizarEstado(bool libre)
     {
-        
+        estaLibre = libre;
+        if (libre)
+        {
+            panelVisual.color = Color.green;
+            estadoTexto.text = "Libre";
+        }
+        else
+        {
+            panelVisual.color = Color.red;
+            estadoTexto.text = "Ocupado";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator AtenderCliente(Cliente cliente, System.Action onTerminar)
     {
-        
+        clienteActual = cliente;
+        ActualizarEstado(false);
+        clientesAtendidos++;
+        float tiempo = cliente.tiempoAtencion;
+        tiempoTotalAtencion += tiempo;
+        yield return new WaitForSeconds(tiempo);
+        clienteActual = null;
+        ActualizarEstado(true);
+        onTerminar?.Invoke();
     }
 }
